f60f44023cf20354e1908bb7a6ab09fb992ab3a5
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-17 08:21:44 +0100

    fixed faker structed class

--- apps/comment/src/app/app.comment.tsx
+++ apps/comment/src/app/app.comment.tsx
@@ -1,24 +1,23 @@
-import {faker} from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 const Comment = () => {
   return (
     <div className="ui container comments">
-      <div className="coment">
+      <div className="comment">
         <a href="/" className="avatar">
-            <img src={faker.image.avatar()} alt="avatar" />
+          <img src={faker.image.avatar()} alt="avatar" />
         </a>
-        <div className="comment">
-            <a href="/"  className="author">
-                Sam
-            </a>
-
-            <div className="metadata">
-                <span className="datae">
-                    Today at 6:00PM
-                </span>
-            </div>
-            <div className="text">
-                Nice blog post!
-            </div>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+                Today at 6:00PM
+            </span>
+          </div>
+          <div className="text">
+            Nice blog post!
+          </div>
         </div>
       </div>
     </div>
