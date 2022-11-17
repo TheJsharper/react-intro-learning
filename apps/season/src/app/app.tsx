29cb780ddabac17cb7f4350a15f5874528886e2e
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-17 15:28:02 +0100

    seasson app

---
+++ apps/season/src/app/app.tsx
@@ -0,0 +1,54 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+import styles from './app.module.scss';
+
+interface Props {
+  products: string[];
+}
+
+interface State {
+  lat: number;
+  long: number;
+  error: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = { lat: 0, long: 0, error: '' };
+  }
+
+  static defaultProps: Props = {
+    products: [],
+  };
+
+  override componentDidMount(): void {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        console.log('My Position', position);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      },
+      (positionError: GeolocationPositionError) => {
+        console.log(positionError);
+        const message = `${positionError.message}`;
+        this.setState({ error: message });
+      }
+    );
+  }
+
+  override render(): React.ReactNode {
+    if (this.state.error.length > 0) {
+      return <div style={{ background: 'red' }}>Error: {this.state.error}</div>;
+    } else
+      return (
+        <div>
+          <h1>Latitude: {this.state.lat}</h1>
+          <h1>Longitude: {this.state.long}</h1>
+        </div>
+      );
+  }
+}
+export default App;
