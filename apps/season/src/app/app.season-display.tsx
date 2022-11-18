b044291869c8ad2bada503f3548df6f668418c62
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-18 10:14:53 +0100

    Display season styled

--- apps/season/src/app/app.season-display.tsx
+++ apps/season/src/app/app.season-display.tsx
@@ -9,7 +9,7 @@ const getSeason = (geolocation: GeoLocationModel, month: number) => {
 };
 
 const getDesctiption :(season:string)=>{text:string, icon:string}= (season:string)=>{
-    return season === 'winter' ? {text:'Burr, it is chilly', icon:'snowflake icon season-icon'}: {text:'Lets hit the beach', icon:'sun icon season-icon'};
+    return season === 'winter' ? {text:'Burr, it is chilly', icon:'snowflake icon season-view'}: {text:'Lets hit the beach', icon:'sun icon season-view'};
 };
 const SeasonDisplay = (props: GeoLocationModel) => {
   const season: string = getSeason(props, new Date().getMonth());
@@ -17,9 +17,9 @@ const SeasonDisplay = (props: GeoLocationModel) => {
   const description = getDesctiption(season);
 
   return <div className='centered-text'>
-    <i className={description.icon } />
-    <h1 className='season-text'> {description.text}</h1>;
-    <i className={description.icon} />
+    <i className={description.icon } style={{gridArea:'icon'}}/>
+    <h1 className='season-text season-view'> {description.text}</h1>;
+    <i className={description.icon} style={{gridArea:'icon-bottom'}}/>
   </div> 
 };
 export default SeasonDisplay;
