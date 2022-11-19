f34899340cbaeefefd78fb212fafb9a1a702e99c
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-11-19 18:19:57 +0100

    rewrite class based component

--- apps/search-pics/src/app/components/app.search-bar.tsx
+++ apps/search-pics/src/app/components/app.search-bar.tsx
@@ -1,13 +1,22 @@
-const SearchBar = () => {
-  return (
-    <div className="ui segment">
-      <form className="ui form">
-        <div className="field">
-          <label htmlFor="search">Search images</label>
-          <input id="search" type="text" />
-        </div>
-      </form>
-    </div>
-  );
-};
-export default SearchBar;
+
+import React from "react";
+
+export default class SearchBar extends React.Component{
+
+    private handleChanges(event:React.ChangeEvent<HTMLInputElement>):void{
+        console.log("==>x", event.target.value);
+    }
+    
+    override render(): React.ReactNode {
+        return (
+            <div className="ui segment">
+              <form className="ui form">
+                <div className="field">
+                  <label htmlFor="search">Search images</label>
+                  <input id="search" type="text" onChange={this.handleChanges}/>
+                </div>
+              </form>
+            </div>
+          );
+    }
+}
