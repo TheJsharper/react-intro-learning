fa763cdb81a1e133f9b27799053e5ea6bc650051
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-11-20 00:59:15 +0100

    trying call external api

--- apps/search-pics/src/app/app.tsx
+++ apps/search-pics/src/app/app.tsx
@@ -2,13 +2,23 @@
 
 import React from 'react';
 import SearchBar from './components/app.search-bar';
+import axios from 'axios';
 
 export default class App extends React.Component<unknown, { term: string }> {
   override state: { term: string } = { term: '' };
 
-  handleSubmitSearch(payload:string): void {
+  handleSubmitSearch(payload: string): void {
     //event.preventDefault();
-    console.log('====>x', payload);
+    const urlBase: string = process.env['NX_UNSPLASH_API_BASE_URL'] as string;
+    const clientId: string = process.env['NX_ACCESS_KEY'] as string;
+   // const secretToken: string = process.env['SECRET_KEY'] as string;
+    /*const data = axios.get(`${urlBase}/search/photos`, {
+      params:{query:payload},
+      headers: {
+        Authorization:`Client-ID ${clientId}`
+      },
+    });*/
+    console.log('====>x', payload, urlBase, clientId, process.env);
     this.setState({ term: 'next value' });
   }
   override render(): React.ReactNode {
