5fff831abc856854e1204c15d19884649505df8e
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-21 09:08:29 +0100

    fixed problem with environment nx

--- apps/search-pics/src/app/app.tsx
+++ apps/search-pics/src/app/app.tsx
@@ -7,18 +7,16 @@ import axios from 'axios';
 export default class App extends React.Component<unknown, { term: string }> {
   override state: { term: string } = { term: '' };
 
-  handleSubmitSearch(payload: string): void {
-    //event.preventDefault();
+ async handleSubmitSearch(payload: string): Promise<void> {
     const urlBase: string = process.env['NX_UNSPLASH_API_BASE_URL'] as string;
-    const clientId: string = process.env['NX_ACCESS_KEY'] as string;
-   // const secretToken: string = process.env['SECRET_KEY'] as string;
-    /*const data = axios.get(`${urlBase}/search/photos`, {
+    const clientId: string = process.env['NX_UNSPLASH_ACCESS_KEY'] as string;
+    const data = axios.get(`${urlBase}/search/photos`, {
       params:{query:payload},
       headers: {
         Authorization:`Client-ID ${clientId}`
       },
-    });*/
-    console.log('====>x', payload, urlBase, clientId, process.env);
+    });/**/
+    console.log('====>x', payload, urlBase, clientId, process.env, await data);
     this.setState({ term: 'next value' });
   }
   override render(): React.ReactNode {
