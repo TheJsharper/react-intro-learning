fd46d40c2ddda37807a2bc495e9b1209c88bbd6c
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-21 11:58:18 +0100

    api call refactored and simple list is shown

--- apps/search-pics/src/app/components/app.image-list.tsx
+++ apps/search-pics/src/app/components/app.image-list.tsx
@@ -8,8 +8,8 @@ export interface ImngeListProps {
 const ImageList: (props: ImngeListProps) => JSX.Element = (
   props: ImngeListProps
 ) => {
-  const images:JSX.Element[]|undefined = props.results?.map((value: Result) => {
-    return <img src={value.urls.regular} alt={value.alt_description || ''} />;
+  const images:JSX.Element[]|undefined = props.results?.map((value: Result, index:number) => {
+    return <img key={index} src={value.urls.regular} alt={value.alt_description || ''} />;
   });
   return <div>{images}</div>;
 };
