0f9e4c88f28cfde1d06936c1c01a6a16071a9066
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-21 10:13:41 +0100

    typed response axios unsplash

--- apps/search-pics/src/app/app.tsx
+++ apps/search-pics/src/app/app.tsx
@@ -1,23 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
+import axios, { AxiosResponse } from 'axios';
 import React from 'react';
+import { UnsplashResponse } from './app.models';
 import SearchBar from './components/app.search-bar';
-import axios from 'axios';
 
-export default class App extends React.Component<unknown, { term: string }> {
-  override state: { term: string } = { term: '' };
+export default class App extends React.Component<unknown, {unsplashResponse:UnsplashResponse|null }> {
+  override state: { unsplashResponse:UnsplashResponse|null } = { unsplashResponse:null };
 
  async handleSubmitSearch(payload: string): Promise<void> {
     const urlBase: string = process.env['NX_UNSPLASH_API_BASE_URL'] as string;
     const clientId: string = process.env['NX_UNSPLASH_ACCESS_KEY'] as string;
-    const data = axios.get(`${urlBase}/search/photos`, {
+    const data:Promise<AxiosResponse<UnsplashResponse>> = axios.get(`${urlBase}/search/photos`, {
       params:{query:payload},
       headers: {
         Authorization:`Client-ID ${clientId}`
       },
-    });/**/
+    });
+    const responseData:UnsplashResponse|null = (await data).data;
     console.log('====>x', payload, urlBase, clientId, process.env, await data);
-    this.setState({ term: 'next value' });
+    this.setState({ unsplashResponse: responseData});
   }
   override render(): React.ReactNode {
     return (
