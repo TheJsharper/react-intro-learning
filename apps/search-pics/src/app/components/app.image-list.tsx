ad3ba91cd255a752aef7e038b7582b18cc67f409
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-22 10:34:32 +0100

    fixed height picture by loading runtime height

--- apps/search-pics/src/app/components/app.image-list.tsx
+++ apps/search-pics/src/app/components/app.image-list.tsx
@@ -1,16 +1,16 @@
 import { Result } from '../app.models';
-import  './app.image-list.module.scss';
+import './app.image-list.module.scss';
+import ImageCard from './image-card';
 
 export interface ImngeListProps {
   results: Result[] | undefined;
 }
 
-const ImageList: (props: ImngeListProps) => JSX.Element = (
-  props: ImngeListProps
-) => {
-  const images:JSX.Element[]|undefined = props.results?.map((value: Result) => {
-    return <img key={value.id} src={value.urls.regular} alt={value.alt_description || ''} />;
-  });
-  return <div className='image-list' >{images}</div>;
+const ImageList: (props: ImngeListProps) => JSX.Element = (props: ImngeListProps) => {
+  const images: JSX.Element[] | undefined = props.results?.map((result: Result) => {
+    return <ImageCard key={result.id} {...result} />;
+  }
+  );
+  return <div className="image-list">{images}</div>;
 };
 export default ImageList;
---
+++ apps/search-pics/src/app/components/image-card.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import React from 'react';
+import { Result } from '../app.models';
+
+export default class ImageCard extends React.Component<Result, { spans: number }>{
+    private imgRef: React.RefObject<HTMLImageElement>;
+    override state = { spans: 0 };
+    constructor(props: Result) {
+        super(props);
+        this.imgRef = React.createRef<HTMLImageElement>()
+    }
+    override componentDidMount(): void {
+        this.imgRef.current?.addEventListener("load", (_: Event) => this.setSpans());
+    }
+    setSpans(): void {
+
+        console.log("ref", this.imgRef.current?.clientHeight);
+        const height = this.imgRef.current?.clientHeight;
+        const spans = Math.ceil(height! /250);
+        this.setState({ spans });
+
+    }
+    override render(): React.ReactNode {
+        const { urls, alt_description } = this.props;
+        return (
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+
+                <img ref={this.imgRef} src={urls.regular} alt={alt_description || ''} />
+            </div>
+        );
+    }
+}
