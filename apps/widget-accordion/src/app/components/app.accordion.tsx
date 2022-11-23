29b8b5f70cf0b1a8208b987bd78f9681d1848edf
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-23 16:12:05 +0100

    accordion component is ready

--- apps/widget-accordion/src/app/components/app.accordion.tsx
+++ apps/widget-accordion/src/app/components/app.accordion.tsx
@@ -1,8 +1,37 @@
-export interface AccordionProps{
-    title:string, content:string;
+import React, { useState } from "react";
+
+export interface AccordionProps {
+    title: string, content: string;
 }
 
-const Accordion = (props:{items:AccordionProps[]})=>{
-    return(<h1>{props.items.length}</h1>)
+const Accordion = (props: { items: AccordionProps[] }) => {
+
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    const onTitleClick = (index:number)=>{
+        console.log('title clicked ', index);
+        setActiveIndex(index);
+    }
+    const rendererItems: Array<JSX.Element> = props.items.map((item, index:number) => {
+        const active = activeIndex === index ? 'active': '';
+        return (
+            <React.Fragment key={item.title}>
+                <div  onClick={ ()=> onTitleClick(index)} className={`title ${active}`}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+
+                <div className={`content  ${active}`}>
+                    <p>
+                        {item.content}
+                    </p>
+                </div>
+            </React.Fragment>
+        );
+    });
+    return (
+        <div className=" ui styled accordion">
+            {rendererItems}
+        </div>)
 }
 export default Accordion;
