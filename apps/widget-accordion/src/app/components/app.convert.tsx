bdfbef63afdad06bed822189b25482c7024db736
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-28 13:36:01 +0100

    getLanguaguesIso

--- apps/widget-accordion/src/app/components/app.convert.tsx
+++ apps/widget-accordion/src/app/components/app.convert.tsx
@@ -11,24 +11,35 @@ export interface ConvertProps {
 const Convert = (props: ConvertProps) => {
     const { language, text } = props;
 
-    const [translated, setTranslated] = useState<Translation[]>([])
+    const [translated, setTranslated] = useState<Translation[]>([]);
+
+    const [debouncedText, setDebouncedText] = useState<string>(text);
+
+    useEffect(()=>{
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+        return ()=> clearTimeout(timerId);
+    }, [text]);
+
+
     useEffect(() => {
 
         (async () => {
-            const {data} = await googleTranslateApiPost({ language, text });
+            const {data} = await googleTranslateApiPost({ language, text:debouncedText });
             console.log("DATA ", data.data.translations);
             setTranslated(data.data.translations);
         })();
 
 
-    }, [language, text]);
+    }, [language, debouncedText]);
 
-const renderedTranslations = translated.map((value:Translation, index:number)=> (
-    <div key={index} >
-            <h2> Language Origin:{value.detectedSourceLanguage} </h2>
-            <h1> Translated: {value.translatedText} </h1>
-    </div>
-))
+        const renderedTranslations = translated.map((value:Translation, index:number)=> (
+            <div key={index} >
+                    <h2> Language Origin:{value.detectedSourceLanguage} </h2>
+                    <h1> Translated: {value.translatedText} </h1>
+            </div>
+        ))
 
 
     return (<div>
