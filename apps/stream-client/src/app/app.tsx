d1df772cc07fb46c8c0ec7260daa1a015feedfc0
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-30 16:57:27 +0100

    react-router added

--- apps/stream-client/src/app/app.tsx
+++ apps/stream-client/src/app/app.tsx
@@ -1,7 +1,46 @@
+import {BrowserRouter, Link, Route } from 'react-router-dom';
+
+const PageOne = ()=>{
+
+  return <div> PageOne</div>
+}
+
+const PageTwo = ()=>{
+
+  return <div> PageTwo
+    <Link to="/"> PageOne</Link>
+  </div>
+}
+
+const PageThree = ()=>{
+
+  return <div> PageThree
+     <Link to="/pagetwo"> PageTwo</Link>
+  </div>
+}
+
+const PageFour = ()=>{
+
+  return <div> PageFour
+    <Link to="/pagetwo"> PageTwo</Link>
+    <Link to="/"> MainHome</Link>
+    <Link to="/pagetwo"> PageTwo</Link>
+  </div>
+}
+
 
 export function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      
+        <Route path="/"  exact component={PageOne}/>
+        <Route path="/pagetwo"  exact component={PageTwo}/>
+        <Route path="/pagethree"  exact component={PageThree}/>
+        <Route path="/pagefour"  exact component={PageFour}/>
+      </BrowserRouter>
+
+    </div>
   );
 }
 
