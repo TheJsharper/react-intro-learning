fc2c9118232d0c6adb6630c4cbaddbee75a07297
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-30 13:27:08 +0100

    fixed problem overfetching data from backend with lodash.memoize util function

--- apps/blog/src/app/actions/index.ts
+++ apps/blog/src/app/actions/index.ts
@@ -2,17 +2,24 @@
 import { Post } from "../models/app.json-placeholder.models";
 import getJsonPlaceholderInstance from "../services/app.jsonplaceholder.service";
 
+import { memoize } from 'lodash';
 export const fetchPosts = () => async (dispatch: any) => {
 
     const response = await getJsonPlaceholderInstance().get<Post[]>('/posts');
-    
+
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
-    
-    
+
+
 }
-export const fetchUser = (id:number) => async (dispatch:any)=>{
+export const fetchUser = (id:number) => async (dispatch:any)=> _memoizedFetchUser(id, dispatch);
+
+
+
+const _memoizedFetchUser = memoize(async(id:number, dispatch:any )=>{
     const response = await getJsonPlaceholderInstance().get<Post[]>(`/users/${id}`);
 
     dispatch({type:'FETCH_USER', payload:response.data});
+});
 
-}
+
+
